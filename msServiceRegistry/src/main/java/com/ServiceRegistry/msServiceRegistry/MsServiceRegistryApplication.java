6ea3dafb07f35f9b7805e9b572382c7fb28feaa5
Samarth-1211 <samarthsharma1211@gmail.com>
2026-01-15 16:33:48 +0530

    Added MicroService Service Registry In Application

---
+++ msServiceRegistry/src/main/java/com/ServiceRegistry/msServiceRegistry/MsServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.ServiceRegistry.msServiceRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsServiceRegistryApplication.class, args);
+	}
+
+}
