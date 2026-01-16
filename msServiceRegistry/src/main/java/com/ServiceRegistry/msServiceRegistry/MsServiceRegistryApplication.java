8494776c8453eab3691d424cd5789ce3b01175bf
Samarth-1211 <samarthsharma1211@gmail.com>
2026-01-16 07:25:06 +0530

    Anootation Added for EnableEurekaServer

--- msServiceRegistry/src/main/java/com/ServiceRegistry/msServiceRegistry/MsServiceRegistryApplication.java
+++ msServiceRegistry/src/main/java/com/ServiceRegistry/msServiceRegistry/MsServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package com.ServiceRegistry.msServiceRegistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsServiceRegistryApplication {
 
 	public static void main(String[] args) {
